f0d018c6a48b14f7dd2b58a8f3298c0442378205
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-06-01 08:11:33 +0200

    ✅ Add Broadcast receiver
    Listen to bluetooth on change event

--- app/src/main/java/fr/thegostsniperfr/android_basic_navigation/MainActivity.kt
+++ app/src/main/java/fr/thegostsniperfr/android_basic_navigation/MainActivity.kt
@@ -1,11 +1,16 @@
 package fr.thegostsniperfr.android_basic_navigation
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import fr.thegostsniperfr.android_basic_navigation.navigation.nav_graph.SetupNavGraph
+import fr.thegostsniperfr.android_basic_navigation.screens.HomeScreen
 import fr.thegostsniperfr.android_basic_navigation.ui.theme.Android_Basic_Navigation_Theme
 
 class MainActivity : ComponentActivity() {
@@ -15,10 +20,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            navController = rememberNavController()
-
             Android_Basic_Navigation_Theme {
-                SetupNavGraph(navController = navController)
+                HomeScreen(
+                    onBluetoothStateChange = {
+                        println("bluetooth state changed")
+                    }
+                )
             }
         }
     }
--- app/src/main/java/fr/thegostsniperfr/android_basic_navigation/screens/HomeScreen.kt
+++ app/src/main/java/fr/thegostsniperfr/android_basic_navigation/screens/HomeScreen.kt
@@ -1,70 +1,17 @@
 package fr.thegostsniperfr.android_basic_navigation.screens
 
-
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import fr.thegostsniperfr.android_basic_navigation.navigation.AUTHENTICATION_ROUTE
-import fr.thegostsniperfr.android_basic_navigation.navigation.Screen
-
+import android.bluetooth.BluetoothAdapter
+import androidx.compose.runtime.Composable
+import fr.thegostsniperfr.android_basic_navigation.broadcast.SystemBroadcastReceiver
 
 @Composable
-fun HomeScreen(
-    navController: NavController,
-) {
-   Box(
-       modifier = Modifier
-           .fillMaxSize()
-           .background(color = Color.Black),
-
-       contentAlignment = Alignment.Center
-   ) {
-       Column (
-           modifier = Modifier
-               .fillMaxSize(),
-           verticalArrangement = Arrangement.Center,
-           horizontalAlignment = Alignment.CenterHorizontally
-
-       ) {
-           Text(
-               text = "Home Page",
-               color = MaterialTheme.colorScheme.primary,
-               fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-               fontWeight = FontWeight.Bold
-           )
-
-           Text(
-               modifier = Modifier
-                   .padding(top = 150.dp)
-                    .clickable {
-                        navController.navigate(AUTHENTICATION_ROUTE)
-                    },
-
-               text = "Login / Sign Up",
-               color = MaterialTheme.colorScheme.primary,
-               fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-               fontWeight = FontWeight.Bold
-           )
-       }
-   }
-}
-
-@Composable
-@Preview(showBackground = true)
-fun HomeScreenPreview() {
-    HomeScreen(
-        navController = rememberNavController()
-    )
+fun HomeScreen (
+    onBluetoothStateChange: () -> Unit
+){
+    SystemBroadcastReceiver(systemAction = BluetoothAdapter.ACTION_STATE_CHANGED) { receiveIntent ->
+        val action = receiveIntent?.action?: return@SystemBroadcastReceiver
+        if(action == BluetoothAdapter.ACTION_STATE_CHANGED) {
+            onBluetoothStateChange()
+        }
+    }
 }
